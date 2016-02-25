bf9cee7a2da47a331619757860412e41f9368a07
You Kobayashi <sweep.3092@gmail.com>
2016-02-25 17:16:28 +0900

    さいどしーあい

--- app/models/banana.rb
+++ app/models/banana.rb
@@ -1,4 +1,5 @@
 class Banana < ActiveRecord::Base
+  attr_accessor :id, :type, :github_repository_url, :github_checkout_key, :before_revision, :after_revision, :branch, :pull_request_number, :base_branch, :depth
   def cut
     "cut cut!"
   end
