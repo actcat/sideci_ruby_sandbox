3377ae26882aa6efebbf9a54069e69f72439dba5
wata727 <watassbass@gmail.com>
2016-12-20 00:23:44 +0900

    fix issue

--- app/controllers/foo_controller.rb
+++ app/controllers/foo_controller.rb
@@ -2,7 +2,6 @@ class FooController < ActionController::Base
   def new_user
     user = User.first(:conditions => ("username = '#{params[:username]}'"))
     user = User.first(:conditions => ("username = '#{params[:username]}'"))
-    user = User.first(:conditions => ("username = '#{params[:username]}'"))
     if user.blank?
       render text: user.username
       return
