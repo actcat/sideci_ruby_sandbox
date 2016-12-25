acb675a9671c12937ebc0f937b2d8f37902408c6
Koichiro Sumi <koichiro.sumi@actcat.co.jp>
2016-12-25 22:35:36 +0900

    Add sample code and querly.yml, That detect these new lines

--- app/controllers/foo_controller.rb
+++ app/controllers/foo_controller.rb
@@ -1,4 +1,4 @@
-class FooController < ActionController::Base  
+class FooController < ActionController::Base
   def new_user
     user = User.first(:conditions => ("username = '#{params[:username]}'"))
     user = User.first(:conditions => ("username = '#{params[:username]}'"))
@@ -9,5 +9,21 @@ class FooController < ActionController::Base
     end
 
     render text: "test"
+  end
+
+  def delete_users
+    User.delete_all if User.exists?(:admin => true, :username => params[:username])
+    render text: "Delete all users by Admin"
+  end
+
+  def update_user
+    user = User.first(:conditions => ("username = '#{params[:username]}'"))
+    user.created = Time.zone.now
+    user.save
+  end
+
+  def user_json
+    user = User.first(:conditions => ("username = '#{params[:username]}'"))
+    JSON.dump(user)
   end
 end
